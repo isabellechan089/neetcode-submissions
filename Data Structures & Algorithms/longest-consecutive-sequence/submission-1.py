7891c354cef2154f51e5d829d9bb2ff4dbf6196f
Isabelle Chan <plumblossompl@gmail.com>
2026-06-19 23:58:49 -0500

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest = -float('inf')
+        left, right = 0,0
+        nums = sorted(list(set(nums)))
+        while right < len(nums) - 1:
+            current = nums[right]
+            nextNum = nums[right+1]
+            right +=1
+            if nextNum - current == 1:
+                current = nextNum
+                diff = right - left +1
+                if diff > longest:
+                    longest = diff
+            else:
+                left = right
+        return longest if longest != -float('inf') else 0
---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest = -float('inf')
+        left, right = 0,0
+        nums = sorted(list(set(nums)))
+        if len(nums) == 1: return 1
+        while right < len(nums) - 1:
+            current = nums[right]
+            nextNum = nums[right+1]
+            right +=1
+            if nextNum - current == 1:
+                current = nextNum
+                diff = right - left +1
+                if diff > longest:
+                    longest = diff
+            else:
+                left = right
+        return longest if longest != -float('inf') else 0
